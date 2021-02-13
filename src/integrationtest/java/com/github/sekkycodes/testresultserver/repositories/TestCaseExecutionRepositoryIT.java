33dcb3d1067f1ca91e62303db90d757ac4ff6b4f
sekkycodes <romansekot@gmail.com>
2021-02-13 16:12:30 +0100

    Addressed code smells identified by sonar

--- src/integrationtest/java/com/github/sekkycodes/testresultserver/repositories/TestCaseExecutionRepositoryIT.java
+++ src/integrationtest/java/com/github/sekkycodes/testresultserver/repositories/TestCaseExecutionRepositoryIT.java
@@ -13,7 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class TestCaseExecutionRepositoryIT {
+class TestCaseExecutionRepositoryIT {
 
   @Autowired
   TestCaseExecutionRepository sut;
--- src/main/java/com/github/sekkycodes/testresultserver/repositories/TestSuiteExecutionRepository.java
+++ src/main/java/com/github/sekkycodes/testresultserver/repositories/TestSuiteExecutionRepository.java
@@ -2,8 +2,6 @@ package com.github.sekkycodes.testresultserver.repositories;
 
 import com.github.sekkycodes.testresultserver.domain.TestSuiteExecution;
 import com.github.sekkycodes.testresultserver.domain.TimeNamePK;
-import com.sun.istack.NotNull;
-import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
