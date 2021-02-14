5885dd160a3e0a3c76df208ee8eddf1fcd334864
sekkycodes <romansekot@gmail.com>
2021-02-14 18:14:08 +0100

    Initial setup of VueJS FE

---
+++ app/test-result-server/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Dashboard />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Dashboard from './components/Dashboard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    Dashboard
+  }
+}
+</script>
+
---
+++ app/test-result-server/src/components/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="dashboard">
+    <h1>Dashboard</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+};
+</script>
+
+<style scoped>
+.dashboard {
+  margin: 5px;
+  color: rgb(125, 125, 125);
+}
+</style>
