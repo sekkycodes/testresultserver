536be86a172e060b5dbc3d8bc9bd091d194cf187
Roman Sekot <roman.sekot@avocodo.com>
2021-03-13 21:21:38 +0100

    Introduced custom queries for id fields

--- src/main/java/com/github/sekkycodes/testresultserver/repositories/TestCaseExecutionRepository.java
+++ src/main/java/com/github/sekkycodes/testresultserver/repositories/TestCaseExecutionRepository.java
@@ -5,6 +5,7 @@ import com.github.sekkycodes.testresultserver.domain.TimeNamePK;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -14,10 +15,13 @@ import org.springframework.stereotype.Repository;
 public interface TestCaseExecutionRepository extends
     MongoRepository<TestCaseExecution, TimeNamePK> {
 
+  @Query("{ '_id': '?0;?1' }")
   Optional<TestCaseExecution> findByIdNameAndIdTime(String name, long time);
 
+  @Query("{ '_id': { $regex: '?0;.*' }}")
   List<TestCaseExecution> findAllByIdName(String name);
 
+  @Query("{ 'suiteName': ?0, '_id': { $regex: '.*;?1' }}")
   List<TestCaseExecution> findAllBySuiteNameAndIdTime(String suiteName, long time);
 
   List<TestCaseExecution> findAllBySuiteName(String suiteName);
--- src/main/java/com/github/sekkycodes/testresultserver/repositories/TestSuiteExecutionRepository.java
+++ src/main/java/com/github/sekkycodes/testresultserver/repositories/TestSuiteExecutionRepository.java
@@ -4,6 +4,7 @@ import com.github.sekkycodes.testresultserver.domain.TestSuiteExecution;
 import com.github.sekkycodes.testresultserver.domain.TimeNamePK;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
@@ -14,5 +15,6 @@ import org.springframework.stereotype.Repository;
 public interface TestSuiteExecutionRepository extends
     MongoRepository<TestSuiteExecution, TimeNamePK>, QuerydslPredicateExecutor<TestSuiteExecution> {
 
+  @Query("{'_id': { $regex: '?0;.*' }}")
   List<TestSuiteExecution> findAllByIdName(String name);
 }
