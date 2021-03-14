25cd85abecaca09e28a8d6a33336682aefdd308e
Roman Sekot <roman.sekot@avocodo.com>
2021-03-14 16:46:19 +0100

    Added modal for Junit Xml upload (unfinished)

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div id="app">
     <header>
-      <div class="navbar navbar-dark bg-dark shadow-sm">
+      <div class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
         <div class="container d-flex justify-content-between">
           <div class="navbar-brand d-flex align-items-center">
             <img id="navbar-logo" src="./assets/trs_logo.svg" alt="logo"/>
             <strong>Test Result Server</strong>
+          </div>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <JUnitXmlUpload />
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -18,11 +25,13 @@
 
 <script>
 import Dashboard from './components/Dashboard.vue'
+import JUnitXmlUpload from "@/components/JUnitXmlUpload";
 
 export default {
   name: 'App',
   components: {
-    Dashboard
+    Dashboard,
+    JUnitXmlUpload
   }
 }
 </script>
