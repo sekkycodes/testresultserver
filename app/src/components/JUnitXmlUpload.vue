40a8add7fe40bdc614d74f316aad5d63e65c6457
Roman Sekot <roman.sekot@avocodo.com>
2021-03-20 09:07:54 +0100

    Added multi-file upload capability

--- app/src/components/JUnitXmlUpload.vue
+++ app/src/components/JUnitXmlUpload.vue
@@ -15,28 +15,28 @@
           <div class="modal-body">
             <div class="form-group">
               <label for="fileSelectionInput">File Selection</label>
-              <input class="form-control-file" id="fileSelectionInput" type="file" name="file"
+              <input class="form-control-file" id="fileSelectionInput" type="file" multiple="multiple" name="file"
                      data-overwrite-initial="false" data-min-file-count="1" ref="fileSelection">
               <small class="form-text text-muted">Select a JUnit XML file to upload.</small>
             </div>
             <div class="form-group">
               <label for="project">Project</label>
-              <input id="project" class="form-control" type="text" v-model="project" />
+              <input id="project" class="form-control" type="text" v-model="project"/>
               <small class="form-text text-muted">The project for which the tests were executed.</small>
             </div>
             <div class="form-group">
               <label for="environment">Environment</label>
-              <input id="environment" class="form-control" type="text" v-model="environment" />
+              <input id="environment" class="form-control" type="text" v-model="environment"/>
               <small class="form-text text-muted">For example "DEV", "TEST", "PROD", ...</small>
             </div>
             <div class="form-group">
               <label for="testType">Test Type</label>
-              <input id="testType" class="form-control" type="text" v-model="testType" />
+              <input id="testType" class="form-control" type="text" v-model="testType"/>
               <small class="form-text text-muted">For example "Unit", "Integration", "E2E", ...</small>
             </div>
             <div class="form-group">
               <label for="executionTimestamp">Execution Timestamp (optional)</label>
-              <input id="executionTimestamp" class="form-control" type="text" v-model="executionTime" />
+              <input id="executionTimestamp" class="form-control" type="text" v-model="executionTime"/>
               <small class="form-text text-muted">Will use timestamp in JUnit XML if present, otherwise falls back to
                 using current time.</small>
             </div>
@@ -57,9 +57,10 @@ export default {
   name: "JUnitXmlUpload",
   methods: {
     onUploadClicked: function () {
+      this.$refs.fileSelection.files.forEach(f => this.uploadFile(f));
+    },
+    uploadFile: function (file) {
       const formData = new FormData();
-      const file = this.$refs.fileSelection.files[0];
-      console.dir(file);
       formData.append("file", file);
       formData.append("project", this.project);
       formData.append("environment", this.environment);
